885b8cd9c6d44710b6403c036c9d171a2d3dce65
zineddineAyoub <58141094+zineddineAyoub@users.noreply.github.com>
2021-02-04 10:37:40 +0100

    fix header of index.php

--- public/index.php
+++ public/index.php
@@ -7,12 +7,6 @@
  * @author   Taylor Otwell <taylor@laravel.com>
  */
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS, post, get');
-header("Access-Control-Max-Age", "3600");
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
-header("Access-Control-Allow-Credentials", "true");
-
 
 define('LARAVEL_START', microtime(true));
 
